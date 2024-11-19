bbe9eee972bc0eeb176576dba70b2bfff3ae439b
Nomex2008 <97978327+Nomex2008@users.noreply.github.com>
2024-11-20 00:28:52 +0200

    Commit 2(useForm)
    Add useForm()

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
+import { useForm } from 'react-hook-form'
 import './App.scss'
 
 function App() {
+
+  const {register, handleSubmit} = useForm()
+
+  const onSubmit = (data: any) => {
+    console.log(data)
+  }
 
   return (
     <>
       <h1>Vite + React</h1>
 
-      <form action="">
-        <input type="text" placeholder='Enter e-mail'/>
+      <form onSubmit={handleSubmit(onSubmit)} action="">
+        <input type="text" placeholder='Enter e-mail' 
+        {...register('e-mail', {
+          required: 'This field is required', 
+        })}/>
         <textarea placeholder='Enter massage:'></textarea>
         <button type='submit'>Send</button>
       </form>
